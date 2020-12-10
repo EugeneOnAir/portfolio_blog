35f3bd1944dc20aabc253e1293ed5a0d011dfa60
EugeneOnAir <johnnybox229@mail.ru>
2020-12-10 19:46:03 +0300

    add

--- js/script.js
+++ js/script.js
@@ -1,11 +1,28 @@
 let burger = document.querySelector('.nav-burger');
 let burgerMenu = document.querySelector('.nav');
-
-burger.addEventListener('click', function(){
+let toggleMenu = function(){
 		burger.classList.toggle('active');
 		burgerMenu.classList.toggle('active'); 
 		document.body.classList.toggle('lock');
+}
+
+
+burger.addEventListener('click', function(e){
+		e.stopPropagation();
+		toggleMenu();
 	});
+
+document.addEventListener('click', function(e) {
+    let target = e.target;
+    let itsMenu = target == burgerMenu || burgerMenu.contains(target);
+    let itsBurger = target == burger;
+    let isMenuActive = burgerMenu.classList.contains('active');
+    
+    if (!itsMenu && !itsBurger && isMenuActive) {
+        toggleMenu();
+    }
+});
+
 
 const burgerLinks = document.querySelectorAll('.nav-link');
 burgerLinks.forEach(burgerLink => {burgerLink.addEventListener('click', function(){
